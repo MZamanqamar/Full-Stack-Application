667f70a001a87e04975cae039bcfb0c122fc1e34
MUHAMMAD ZAMAN QAMAR <114555927+MZamanqamar@users.noreply.github.com>
2025-07-12 13:29:52 +0500

    Add files via upload

---
+++ Frontend/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ProductDashboard from './components/ProductDashboard';
+
+function App() {
+  return (
+    <div>
+      <h1>Product Store</h1>
+      <ProductDashboard />
+    </div>
+  );
+}
+
+export default App;
